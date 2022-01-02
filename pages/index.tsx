1d62b6d685a8a0854eaef2840408d7ad631e6301
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-03 07:22:52 +0900

    Wiring up Ethereum

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,33 @@
 import type { NextPage } from 'next'
+import { useEffect, useState, useCallback } from 'react'
+import detectEthereumProvider from '@metamask/detect-provider'
+import CloseSeaContract from '../truffle/abis/CloseSea.json'
+import { ethers } from 'ethers'
+import { ExternalProvider } from '@ethersproject/providers'
 
 const Home: NextPage = () => {
+
+
+  const getProvider = async () => {
+    const provider = await detectEthereumProvider() as ExternalProvider
+    if (provider !== window.ethereum) console.error('Do you have multiple wallets installed?')
+    return new ethers.providers.Web3Provider(provider)
+  }
+
+  const getAccount = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    return accounts[0]
+  }
+
+  useEffect(() => {
+    const loadEthereum = async () => {
+      const provider = await getProvider()
+      const account = await getAccount()
+    }
+    loadEthereum()
+  }, [])
+
+
   return (
    <div>
      HOME
