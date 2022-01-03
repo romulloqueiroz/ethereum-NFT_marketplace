beda661e268be5ec57e393dd2aecaf21eecdf968
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-03 19:47:46 +0900

    Deploying to Ropsten via Infura

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+const mnemonic = 'Secret...Do not look 😉'
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,12 @@ module.exports = {
       port: 7545,
       network_id: '*', 
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'wss://ropsten.infura.io/ws/v3/b49da0cebeee467fb4415bc0df7cd08c')
+      },
+      network_id: 3
+    }
   },
 
   contracts_directory: './contracts/',
