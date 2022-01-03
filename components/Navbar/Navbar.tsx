6918add5e579917f26e5759cf124fa46d51ad479
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-03 09:40:03 +0900

    Document & types & logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,14 +4,35 @@ import styled from 'styled-components'
 const Navbar = () => {
   return (
     <StyledNavbar>
-      Navbar
+      <Logo 
+        src='assets/logo.png'
+        alt='CloseSea Logo'
+      />
+      <Title>
+        CloseSea
+      </Title>
     </StyledNavbar>
   )
 }
 
+const Title = styled.h1`
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+  margin-left: 10px;
+  font-size: 1.4rem;
+`
+
+const Logo = styled.img`
+  height: 40px;
+  width: 40px;
+`
+
 const StyledNavbar = styled.nav`
   height: 72px;
   box-shadow: rgb(4 17 29 / 25%) 0px 0px 8px 0px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
 `
 
 export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from 'next/app'
 import Global from '../styles/Global'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <title>CloseSea, the smallest NFT marketplace</title>
+    </Head>
     <Global />
     <Component {...pageProps} />
   </>
